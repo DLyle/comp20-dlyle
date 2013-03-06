73add8fc591479b8533eb4c1582aecfd6c8ce828
DLyle <david.lyle@tufts.edu>
2013-03-06 17:07:46 -0500

    info windows display schedule

--- where/where.js
+++ where/where.js
@@ -1,30 +1,57 @@
-aleToBt =[{"station":"Alewife","latitude":42.395428, "longitude": -71.142483},
-{"station":"Davis", "latitude":42.39674 , "longitude": -71.121815},
-{"station":"Porter", "latitude":42.3884 , "longitude": -71.119149},
-{"station":"Harvard", "latitude":42.373362 , "longitude": -71.118956},
-{"station":"Central", "latitude":42.365486 , "longitude": -71.103802},
-{"station":"Kendall", "latitude":42.36249079 , "longitude":-71.08617653},
-{"station":"Charles MGH", "latitude":42.361166 , "longitude":-71.070628},
-{"station":"Park", "latitude":42.35639457 , "longitude":-71.0624242},
-{"station":"Downtown Crossing", "latitude":42.355518 , "longitude":-71.060225},
-{"station":"South Station", "latitude":42.352271 , "longitude":-71.055242},
-{"station":"Broadway", "latitude":42.342622 , "longitude":-71.056967},
-{"station":"Andrew", "latitude":42.330154 , "longitude":-71.057655},
-{"station":"JFK", "latitude":42.320685 , "longitude":-71.052391},
-{"station":"North Quincy", "latitude":42.275275 , "longitude":-71.029583},
-{"station":"Wollaston", "latitude":42.2665139 , "longitude":-71.0203369},
-{"station":"Quincy Center", "latitude":42.251809 , "longitude":-71.005409},
-{"station":"Quincy Adams", "latitude":42.233391 , "longitude":-71.007153},
-{"station":"Braintree", "latitude":42.2078543 , "longitude":-71.0011385}];      
+aleToBt =[
+{"station":"Alewife","latitude":42.395428, "longitude": -71.142483,
+  "key":"ALE"},
+{"station":"Davis", "latitude":42.39674 , "longitude": -71.121815, 
+  "key":"DAV"},
+{"station":"Porter", "latitude":42.3884 , "longitude": -71.119149,
+  "key":"POR"},
+{"station":"Harvard", "latitude":42.373362 , "longitude": -71.118956,
+  "key":"HAR"},
+{"station":"Central", "latitude":42.365486 , "longitude": -71.103802,
+  "key":"CEN"},
+{"station":"Kendall", "latitude":42.36249079 , "longitude":-71.08617653,
+  "key":"KEN"},
+{"station":"Charles MGH", "latitude":42.361166 , "longitude":-71.070628,
+  "key":"MGH"},
+{"station":"Park", "latitude":42.35639457 , "longitude":-71.0624242,
+  "key":"PRK"},
+{"station":"Downtown Crossing", "latitude":42.355518 , "longitude":-71.060225,
+  "key":"DTC"},
+{"station":"South Station", "latitude":42.352271 , "longitude":-71.055242,
+  "key":"SOU"},
+{"station":"Broadway", "latitude":42.342622 , "longitude":-71.056967,
+  "key":"BRO"},
+{"station":"Andrew", "latitude":42.330154 , "longitude":-71.057655,
+  "key":"AND"},
+{"station":"JFK", "latitude":42.320685 , "longitude":-71.052391,
+  "key":"JFK"},
+{"station":"North Quincy", "latitude":42.275275 , "longitude":-71.029583,
+  "key":"NQU"},
+{"station":"Wollaston", "latitude":42.2665139 , "longitude":-71.0203369,
+  "key":"WOL"},
+{"station":"Quincy Center", "latitude":42.251809 , "longitude":-71.005409,
+  "key":"QUC"},
+{"station":"Quincy Adams", "latitude":42.233391 , "longitude":-71.007153,
+  "key":"QUA"},
+{"station":"Braintree", "latitude":42.2078543 , "longitude":-71.0011385,
+  "key":"BRA"}];      
 
-jfkToAsh = [{"station":"JFK", "latitude":42.320685 , "longitude":-71.052391},
-{"station":"Savin Hill", "latitude":42.31129 , "longitude":-71.053331},
-{"station":"Fields Corner", "latitude":42.300093 , "longitude":-71.061667},
-{"station":"Shawmut", "latitude":42.29312583 , "longitude":-71.06573796},
-{"station":"Ashmont", "latitude":42.284652 , "longitude":-71.064489}];
+jfkToAsh = [{"station":"JFK", "latitude":42.320685 , "longitude":-71.052391,
+  "key":"JFK"},
+{"station":"Savin Hill", "latitude":42.31129 , "longitude":-71.053331,
+  "key":"SAV"},
+{"station":"Fields Corner", "latitude":42.300093 , "longitude":-71.061667,
+  "key":"FIE"},
+{"station":"Shawmut", "latitude":42.29312583 , "longitude":-71.06573796,
+  "key":"SHA"},
+{"station":"Ashmont", "latitude":42.284652 , "longitude":-71.064489,
+  "key":"ASH"}];
 
 var stations = [];
 var map;
+var rootline = [];
+var ashline = [];
+var windows = {};
 
 function init(){
   console.log("in init");
@@ -46,35 +73,15 @@ function drawMap(){
 function handleError(position){
   alert("Failed to find position");
 }
-function createWindow(marker){
-  var infowindow = new google.maps.InfoWindow();
+function createWindow(marker,key){
+    windows[key] = new google.maps.InfoWindow();
+    windows[key].setContent(marker.getTitle());
     google.maps.event.addListener(marker, 'click',function(){
-        infowindow.setContent(marker.getTitle());
-        infowindow.open(map,marker);
+        windows[key].open(map,marker);
         });
 }
 
-function drawRedline(){
-var rootline = [];
-var ashline = [];
-  for(i=0;i<aleToBt.length;i++){
-    rootline[i] = 
-     new google.maps.LatLng(aleToBt[i]["latitude"],aleToBt[i]["longitude"]);
-    var marker = new google.maps.Marker({
-                      position:rootline[i],
-                      map:map,icon:'Tstop.png'});
-    marker.setTitle(aleToBt[i]["station"]);
-    createWindow(marker);
-  }
-  for(j=0;j < jfkToAsh.length;j++){
-    ashline[j] = 
-     new google.maps.LatLng(jfkToAsh[j]["latitude"],jfkToAsh[j]["longitude"]);
-    var marker = new google.maps.Marker({
-        position:ashline[j],
-        map:map,icon:'Tstop.png'});
-    marker.setTitle(jfkToAsh[j]["station"]);
-    createWindow(marker);
-  }
+function drawPolyLines(){
   redline1 = new google.maps.Polyline({
             path:rootline,
             map:map,
@@ -83,7 +90,29 @@ var ashline = [];
             path:ashline,
             map:map,
             strokeColor:"FF0000"});
-  //fillMarkers()
+}
+
+function drawRedline(){
+  for(i=0;i<aleToBt.length;i++){
+    rootline[i] = 
+     new google.maps.LatLng(aleToBt[i]["latitude"],aleToBt[i]["longitude"]);
+    var marker = new google.maps.Marker({
+                      position:rootline[i],
+                      map:map,icon:'Tstop.png'});
+    marker.setTitle(aleToBt[i]["station"]);
+    createWindow(marker,aleToBt[i]["key"]);
+  }
+  for(j=0;j < jfkToAsh.length;j++){
+    ashline[j] = 
+     new google.maps.LatLng(jfkToAsh[j]["latitude"],jfkToAsh[j]["longitude"]);
+    var marker = new google.maps.Marker({
+        position:ashline[j],
+        map:map,icon:'Tstop.png'});
+    marker.setTitle(jfkToAsh[j]["station"]);
+    createWindow(marker,jfkToAsh[j]["key"]);
+  }
+  drawPolyLines();
+  fillSched();
 }
 
 function getMyLocation(){
@@ -101,7 +130,7 @@ function getMyLocation(){
   }
 }
 
-function fillMarkers(){
+function fillSched(){
   try{
     request = new XMLHttpRequest();
     request.open("GET",
@@ -116,9 +145,12 @@ function fillMarkers(){
     if(request.readyState === 4 && request.status === 200){
       sched = JSON.parse(request.responseText);
       for(i = 0; i < sched.length; i++){
-        var marker = stations[sched[i]["PlatformKey"]]; 
-
+        var key = sched[i]["PlatformKey"];
+        key = key.substring(1,key.length-1);
+        var cont = windows[key].getContent();
+        windows[key].setContent(cont.concat("<br>",sched[i]["Time"]));
       }
     }
   }
 }
+
